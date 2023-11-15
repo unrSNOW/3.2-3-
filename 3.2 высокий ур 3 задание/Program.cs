f5f6f2699f6e6eea322c417a3647e2925bc2ea9e
Asus <Asus@LAPTOP-4QTPOURE>
2023-11-15 18:50:00 +0300

    Добавьте файлы проекта.

---
+++ 3.2 высокий ур 3 задание/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите натуральное число:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("числа удовлетворяющие условию:");
+for (int i = 1; i <= n; i++)
+{
+    int square = i * i;
+    string number = i.ToString();
+    string lastsquare = square.ToString().Substring(square.ToString().Length-number.Length);
+    if (number == lastsquare) Console.WriteLine(i);
+}
